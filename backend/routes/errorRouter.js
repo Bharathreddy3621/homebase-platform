17e6d792af13b3ca1a8ccf615b1bf7cffdf90f8f
Bharath Reddy <186083834+Bharathreddy3621@users.noreply.github.com>
2026-06-13 12:00:23 +0530

    remove legacy views

--- backend/routes/errorRouter.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import * as errorsController from "../controllers/errors.js";
-
-const errorRouter = express.Router();
-
-errorRouter.get("/404", errorsController.pageNotFound);
-
-export default errorRouter;
